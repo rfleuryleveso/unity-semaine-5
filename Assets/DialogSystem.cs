ea09fdc99698a1a215ab7518ac3a3e1fc40c24e0
Raphael FLEURY-LE VESO <mail@rflv.fr>
2022-10-23 22:45:55 +0200

    first commit

---
+++ Assets/DialogSystem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogSystem : MonoBehaviour
+{
+    [SerializeField] private Discussion discussion;
+    [SerializeField] private TMP_Text dialogDisplay;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Button previousButton;
+
+    private int dialogIndex = 0;
+
+    private void Start()
+    {
+        nextButton.onClick.AddListener(this.HandleNextButton);
+        previousButton.onClick.AddListener(this.HandlePreviousButton);
+        this.UpdateDialogDisplay(dialogIndex);
+    }
+
+    private void UpdateDialogDisplay(int index)
+    {
+        if (index >= 0 && index < discussion.dialogues.Count)
+        {
+            dialogDisplay.text = discussion.dialogues[index];
+        }
+
+        this.nextButton.enabled = index < discussion.dialogues.Count - 1;
+        this.previousButton.enabled = index > 0;
+    }
+
+    private void HandleNextButton()
+    {
+        if (this.dialogIndex + 1 <= (discussion.dialogues.Count - 1))
+        {
+            this.dialogIndex++;
+        }
+
+        this.UpdateDialogDisplay(this.dialogIndex);
+    }
+
+    private void HandlePreviousButton()
+    {
+        if (this.dialogIndex - 1 >= 0)
+        {
+            this.dialogIndex--;
+        }
+
+        this.UpdateDialogDisplay(this.dialogIndex);
+    }
+}
---
+++ Assets/Discussion.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "discussion", menuName = "Discussion")]
+public class Discussion : ScriptableObject
+{
+    public List<string> dialogues;
+}
